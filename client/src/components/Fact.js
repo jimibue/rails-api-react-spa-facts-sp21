0b17309809eb761b1b33bd877006c9614a2d28c8
James Yeates <jimibue@gmail.com>
2021-03-08 11:11:25 -0700

    fact form dymanic

--- client/src/components/Fact.js
+++ client/src/components/Fact.js
@@ -1,8 +1,12 @@
+import FactForm from "./FactForm"
+
 const Fact = (props) =>{
     const {id, text, source, username} = props
     return (
+
         <div className='fact-container'>
             <h1>id{id}: {text}</h1>
+            <FactForm id={id} text={text} source={source} username={username}/>
         </div>
     )
 }
--- client/src/components/FactForm.js
+++ client/src/components/FactForm.js
@@ -1,23 +1,41 @@
 import {useState} from 'react'
 const FactForm = (props) => {
-    const [text, setText] = useState('')
+    const {id, text: initText, username: initUsername, source: initSource} = props
+    const [text, setText] = useState(initText ? initText: '' )
+    const [username, setUsername] = useState(initUsername ? initUsername : '')
+    const [source, setSource] = useState(initSource ? initSource : '')
      
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('submited')
-        console.log(text)
+        console.log({text, source, username})
+        if(id){
+            console.log('update axios call here')
+        } else {
+            console.log('create call')
+        }
     }
     return(
-        <>
+        <div className='form-container'>
+          <h1>{id ? 'Edit' : 'New'}</h1>
           <form onSubmit={handleSubmit}>
                <p>Enter Fact</p>
                <input 
                  value={text} 
                  onChange={(e)=> setText(e.target.value)}
                 />
-              <button>submit</button>
+               <p>Enter UserName</p>
+               <input 
+                 value={username} 
+                 onChange={(e)=> setUsername(e.target.value)}
+                />
+              <p>Enter source</p>
+               <input 
+                 value={source} 
+                 onChange={(e)=> setSource(e.target.value)}
+                />
+              <button>{id ? 'Update' : 'Create'}</button>
           </form>
-        </>
+        </div>
     )
 }
 
